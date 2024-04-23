3662087900d0f1f9abb6aea77713f37b5630e5de
qriwty <arthur.social.rech@gmail.com>
2024-04-23 22:45:35 +0300

    Controller update

--- webots/controllers/ardupilot_vehicle_controller/ardupilot_vehicle_controller.py
+++ webots/controllers/ardupilot_vehicle_controller/ardupilot_vehicle_controller.py
@@ -1,79 +1,23 @@
 #!/usr/bin/env python3
 import time
-import toml
 from webots_vehicle import WebotsArduVehicle
-
-
-configuration_file = "arducopter_configuration.toml"
-configuration = toml.load(configuration_file)
-
-
-def get_configuration_value(section, key, default=None):
-    return configuration.get(section, {}).get(key, default)
-
-
-motor_names = get_configuration_value("powerplant", "motors")
-motor_cap = get_configuration_value("powerplant", "motor_cap", float("inf"))
-reversed_motors = get_configuration_value("powerplant", "reversed", False)
-bidirectional_motors = get_configuration_value("powerplant", "bidirectional", False)
-uses_propellers = get_configuration_value("powerplant", "uses_propellers", True)
-
-accel_name = get_configuration_value("sensors", "accel", "accelerometer")
-imu_name = get_configuration_value("sensors", "imu", "inertial unit")
-gyro_name = get_configuration_value("sensors", "gyro", "gyro")
-gps_name = get_configuration_value("sensors", "gps", "gps")
-
-gimbal_roll = get_configuration_value("gimbal", "roll", "camera roll")
-gimbal_pitch = get_configuration_value("gimbal", "pitch", "camera roll")
-gimbal_yaw = get_configuration_value("gimbal", "yaw", "camera roll")
-
-gimbal_roll_sensor = get_configuration_value("gimbal", "roll_sensor", "camera roll sensor")
-gimbal_pitch_sensor = get_configuration_value("gimbal", "pitch_sensor", "camera roll sensor")
-gimbal_yaw_sensor = get_configuration_value("gimbal", "yaw_sensor", "camera roll sensor")
-
-camera_name = get_configuration_value("camera", "name", "")
-camera_mode = get_configuration_value("camera", "mode", "color")
-camera_fps = get_configuration_value("camera", "fps", 30)
-camera_host = get_configuration_value("camera", "host", "localhost")
-camera_port = get_configuration_value("camera", "port", None)
-
-rangefinder_name = get_configuration_value("rangefinder", "name", "")
-rangefinder_fps = get_configuration_value("rangefinder", "fps", 30)
-rangefinder_host = get_configuration_value("rangefinder", "host", "localhost")
-rangefinder_port = get_configuration_value("rangefinder", "port", None)
-
-instance = get_configuration_value("sitl", "instance", 0)
-sitl_address = get_configuration_value("sitl", "sitl_address", "127.0.0.1")
 
 
 if __name__ == "__main__":
     vehicle = WebotsArduVehicle(
-        motor_names=motor_names,
-        reversed_motors=reversed_motors,
-        accel_name=accel_name,
-        imu_name=imu_name,
-        gyro_name=gyro_name,
-        gps_name=gps_name,
-        gimbal_roll=gimbal_roll,
-        gimbal_pitch=gimbal_pitch,
-        gimbal_yaw=gimbal_yaw,
-        gimbal_roll_sensor=gimbal_roll_sensor,
-        gimbal_pitch_sensor=gimbal_pitch_sensor,
-        gimbal_yaw_sensor=gimbal_yaw_sensor,
-        camera_name=camera_name,
-        camera_fps=camera_fps,
-        camera_stream_host=camera_host,
-        camera_stream_port=camera_port,
-        camera_mode=camera_mode,
-        rangefinder_name=rangefinder_name,
-        rangefinder_fps=rangefinder_fps,
-        rangefinder_stream_host=rangefinder_host,
-        rangefinder_stream_port=rangefinder_port,
-        instance=instance,
-        motor_velocity_cap=motor_cap,
-        bidirectional_motors=bidirectional_motors,
-        uses_propellers=uses_propellers,
-        sitl_address=sitl_address
+        motor_names=["m1_motor", "m2_motor", "m3_motor", "m4_motor"],
+        accel_name="accelerometer",
+        imu_name="inertial unit",
+        gyro_name="gyro",
+        gps_name="gps",
+        gimbal_roll="camera roll",
+        gimbal_pitch="camera pitch",
+        gimbal_yaw="camera yaw",
+        color_camera_name="color_camera",
+        depth_camera_name="depth_camera",
+        rangefinder_name="rangefinder",
+        sitl_address="127.0.0.1",
+        instance=0,
     )
 
     while vehicle.webots_connected():
--- webots/controllers/ardupilot_vehicle_controller/webots_vehicle.py
+++ webots/controllers/ardupilot_vehicle_controller/webots_vehicle.py
@@ -12,9 +12,9 @@ import socket
 import select
 import struct
 import numpy as np
-import math
+import zmq
 from threading import Thread
-from typing import List, Union
+from typing import List
 
 from pymavlink import mavutil
 from pymavlink.quaternion import QuaternionBase
@@ -55,71 +55,25 @@ class WebotsArduVehicle:
 
     def __init__(self,
                  motor_names: List[str],
-                 accel_name: str = "accelerometer",
-                 imu_name: str = "inertial unit",
-                 gyro_name: str = "gyro",
-                 gps_name: str = "gps",
-                 gimbal_roll: str = None,
-                 gimbal_pitch: str = None,
-                 gimbal_yaw: str = None,
-                 gimbal_roll_sensor: str = None,
-                 gimbal_pitch_sensor: str = None,
-                 gimbal_yaw_sensor: str = None,
-                 camera_name: str = None,
-                 camera_mode: str = "color",
-                 camera_fps: int = 30,
-                 camera_stream_port: int = None,
-                 camera_stream_host: int = None,
-                 rangefinder_name: str = None,
-                 rangefinder_fps: int = 30,
-                 rangefinder_stream_host: int = None,
-                 rangefinder_stream_port: int = None,
+                 accel_name: str,
+                 imu_name: str,
+                 gyro_name: str,
+                 gps_name: str,
+                 gimbal_roll: str,
+                 gimbal_pitch: str,
+                 gimbal_yaw: str,
+                 color_camera_name: str,
+                 depth_camera_name: str,
+                 rangefinder_name: str,
+                 data_stream_fps: int = 30,
+                 data_stream_port: int = 5588,
                  instance: int = 0,
-                 motor_velocity_cap: float = float('inf'),
-                 reversed_motors: List[int] = None,
-                 bidirectional_motors: bool = False,
-                 uses_propellers: bool = True,
                  sitl_address: str = "127.0.0.1"):
-        """WebotsArduVehicle constructor
-
-        Args:
-            motor_names (List[str]): Motor names in ArduPilot numerical order (first motor is SERVO1 etc).
-            accel_name (str, optional): Webots accelerometer name. Defaults to "accelerometer".
-            imu_name (str, optional): Webots imu name. Defaults to "inertial unit".
-            gyro_name (str, optional): Webots gyro name. Defaults to "gyro".
-            gps_name (str, optional): Webots GPS name. Defaults to "gps".
-            camera_name (str, optional): Webots camera name. Defaults to None.
-            camera_fps (int, optional): Camera FPS. Lower FPS runs better in sim. Defaults to 10.
-            camera_stream_host (str, optional): Host to stream camera images to.
-                                                Defaults to localhost.
-            camera_stream_port (int, optional): Port to stream camera images to.
-                                                If no port is supplied the camera will not be streamed. Defaults to None.
-            rangefinder_name (str, optional): Webots RangeFinder name. Defaults to None.
-            rangefinder_fps (int, optional): RangeFinder FPS. Lower FPS runs better in sim. Defaults to 10.
-            rangefinder_stream_host (str, optional): Host to stream rangefinder images to.
-                                                     Defaults to localhost.
-            rangefinder_stream_port (int, optional): Port to stream rangefinder images to.
-                                                     If no port is supplied the camera will not be streamed. Defaults to None.
-            instance (int, optional): Vehicle instance number to match the SITL. This allows multiple vehicles. Defaults to 0.
-            motor_velocity_cap (float, optional): Motor velocity cap. This is useful for the crazyflie
-                                                  which default has way too much power. Defaults to float('inf').
-            reversed_motors (list[int], optional): Reverse the motors (indexed from 1). Defaults to None.
-            bidirectional_motors (bool, optional): Enable bidirectional motors. Defaults to False.
-            uses_propellers (bool, optional): Whether the vehicle uses propellers.
-                                              This is important as we need to linearize thrust if so. Defaults to True.
-            sitl_address (str, optional): IP address of the SITL (useful with WSL2 eg \"172.24.220.98\").
-                                          Defaults to "127.0.0.1".
-        """
         # init class variables
-        self.motor_velocity_cap = motor_velocity_cap
+        self.motor_velocity_cap = float("inf")
         self._instance = instance
-        self._reversed_motors = reversed_motors
-        self._bidirectional_motors = bidirectional_motors
-        self._uses_propellers = uses_propellers
         self._webots_connected = True
-        self._camera_mode = camera_mode
 
-        # setup Webots robot instance
         self.robot = Robot()
 
         # set robot time step relative to sim time step
@@ -140,37 +94,36 @@ class WebotsArduVehicle:
         self.gimbal_pitch = self.robot.getDevice(gimbal_pitch)
         self.gimbal_yaw = self.robot.getDevice(gimbal_yaw)
 
-        self.gimbal_roll_sensor = self.robot.getDevice(gimbal_roll_sensor)
-        self.gimbal_pitch_sensor = self.robot.getDevice(gimbal_pitch_sensor)
-        self.gimbal_yaw_sensor = self.robot.getDevice(gimbal_yaw_sensor)
-
-        self.gimbal_roll_sensor.enable(self._timestep)
-        self.gimbal_pitch_sensor.enable(self._timestep)
-        self.gimbal_yaw_sensor.enable(self._timestep)
+        self.gimbal_roll.getPositionSensor().enable(self._timestep)
+        self.gimbal_pitch.getPositionSensor().enable(self._timestep)
+        self.gimbal_yaw.getPositionSensor().enable(self._timestep)
 
         self.gimbal_roll.setPosition(0)
         self.gimbal_pitch.setPosition(0)
         self.gimbal_yaw.setPosition(0)
 
-        self.camera = self.robot.getDevice(camera_name)
-        self.camera.enable(1000 // camera_fps)  # takes frame period in ms
+        self.color_camera = self.robot.getDevice(color_camera_name)
+        self.color_camera.enable(1000 // data_stream_fps)  # takes frame period in ms
+
+        self.depth_camera = self.robot.getDevice(depth_camera_name)
+        self.depth_camera.enable(1000 // data_stream_fps)
 
         self.rangefinder = self.robot.getDevice(rangefinder_name)
-        self.rangefinder.enable(1000 // camera_fps)
+        self.rangefinder.enable(1000 // data_stream_fps)
 
         self._data_thread = Thread(daemon=True,
                                    target=self._handle_stream,
-                                   args=[camera_stream_host, camera_stream_port])
+                                   args=[data_stream_port])
         self._data_thread.start()
 
         self._camera_data_type = "uint8"
-        self._rangefinder_data_type = "uint8"
+        self._rangefinder_data_type = "uint16"
 
         # init motors (and setup velocity control)
         self._motors = [self.robot.getDevice(n) for n in motor_names]
-        for m in self._motors:
-            m.setPosition(float('inf'))
-            m.setVelocity(0)
+        for motor in self._motors:
+            motor.setPosition(float('inf'))
+            motor.setVelocity(0)
 
         # start ArduPilot SITL communication thread
         self._sitl_thread = Thread(daemon=True, target=self._handle_sitl, args=[sitl_address, 9002+10*instance])
@@ -306,28 +259,13 @@ class WebotsArduVehicle:
             command (tuple): tuple of motor speeds 0.0-1.0 where -1.0 is unused
         """
 
-        # self._handle_gimbal(command[len(self._motors):len(self._motors) + 3])
-
         # get only the number of motors we have
         command_motors = command[:len(self._motors)]
         if -1 in command_motors:
             print(f"Warning: SITL provided {command.index(-1)} motors "
                   f"but model specifies {len(self._motors)} (I-{self._instance})")
 
-        # scale commands to -1.0-1.0 if the motors are bidirectional (ex rover wheels)
-        if self._bidirectional_motors:
-            command_motors = [v*2-1 for v in command_motors]
-
-        # linearize propeller thrust for `MOT_THST_EXPO=0`
-        if self._uses_propellers:
-            # `Thrust = thrust_constant * |omega| * omega` (ref https://cyberbotics.com/doc/reference/propeller)
-            # if we set `omega = sqrt(input_thottle)` then `Thrust = thrust_constant * input_thottle`
-            linearized_motor_commands = [np.sqrt(np.abs(v))*np.sign(v) for v in command_motors]
-
-        # reverse motors if desired
-        if self._reversed_motors:
-            for m in self._reversed_motors:
-                linearized_motor_commands[m-1] *= -1
+        linearized_motor_commands = [np.sqrt(np.abs(v))*np.sign(v) for v in command_motors]
 
         # set velocities of the motors in Webots
         for i, m in enumerate(self._motors):
@@ -348,31 +286,43 @@ class WebotsArduVehicle:
             roll=GimbalAxisData(
                 min=self.gimbal_roll.getMinPosition(),
                 max=self.gimbal_roll.getMaxPosition(),
-                current=self.gimbal_roll_sensor.getValue(),
+                current=self.gimbal_roll.getPositionSensor().getValue(),
                 target=self.gimbal_roll.getTargetPosition()
             ),
             pitch=GimbalAxisData(
                 min=self.gimbal_pitch.getMinPosition(),
                 max=self.gimbal_pitch.getMaxPosition(),
-                current=self.gimbal_pitch_sensor.getValue(),
+                current=self.gimbal_roll.getPositionSensor().getValue(),
                 target=self.gimbal_pitch.getTargetPosition()
             ),
             yaw=GimbalAxisData(
                 min=self.gimbal_yaw.getMinPosition(),
                 max=self.gimbal_yaw.getMaxPosition(),
-                current=self.gimbal_yaw_sensor.getValue(),
+                current=self.gimbal_roll.getPositionSensor().getValue(),
                 target=self.gimbal_yaw.getTargetPosition()
             )
         )
 
         camera_data = CameraData(
             timestamp=self.robot.getTime(),
-            width=self.camera.getWidth(),
-            height=self.camera.getHeight(),
-            fps=1000 / self.camera.getSamplingPeriod(),
-            fov=self.camera.getFov(),
+            width=self.color_camera.getWidth(),
+            height=self.color_camera.getHeight(),
+            fps=1000 / self.color_camera.getSamplingPeriod(),
+            fov=self.color_camera.getFov(),
             data_type=self._camera_data_type,
             frame=self.get_camera_frame()
+        )
+
+        depth_data = RangefinderData(
+            timestamp=self.robot.getTime(),
+            width=self.depth_camera.getWidth(),
+            height=self.depth_camera.getHeight(),
+            fps=1000 / self.depth_camera.getSamplingPeriod(),
+            fov=self.depth_camera.getFov(),
+            min_range=self.depth_camera.getMinRange(),
+            max_range=self.depth_camera.getMaxRange(),
+            data_type=self._rangefinder_data_type,
+            frame=self.get_rangefinder_frame(self.depth_camera)
         )
 
         rangefinder_data = RangefinderData(
@@ -384,7 +334,7 @@ class WebotsArduVehicle:
             min_range=self.rangefinder.getMinRange(),
             max_range=self.rangefinder.getMaxRange(),
             data_type=self._rangefinder_data_type,
-            frame=self.get_rangefinder_frame()
+            frame=self.get_rangefinder_frame(self.rangefinder)
         )
 
         drone_data = DroneData(
@@ -392,50 +342,33 @@ class WebotsArduVehicle:
             fdm=fdm_data,
             gimbal=gimbal_data,
             camera=camera_data,
+            depth=depth_data,
             rangefinder=rangefinder_data
         )
 
         return drone_data
 
-    def _handle_stream(self, host: str, port: int):
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind((host, port))
-        server.listen(1)
+    def _handle_stream(self, port: int):
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind(f"tcp://*:{port}")
 
-        # continuously send images
         while self._webots_connected:
-            # wait for incoming connection
-            conn, _ = server.accept()
-            print(f"Connected to camera client (I-{self._instance})")
+            start_time = self.robot.getTime()
 
-            try:
-                while self._webots_connected:
-                    # delay at sample rate
-                    start_time = self.robot.getTime()
+            message = socket.recv_string()
+            if message == "get_data":
+                data = self._get_drone_data()
+                serialized_data = data.to_json()
+                socket.send_json(serialized_data)
 
-                    data = self._get_drone_data()
-                    serialized_data = data.to_json()
-
-                    separator = "\n"
-
-                    conn.sendall((serialized_data + separator).encode("utf-8"))
-
-                    # delay at sample rate
-                    while self.robot.getTime() - start_time < self.rangefinder.getSamplingPeriod() / 1000:
-                        time.sleep(0.001)
-
-            except ConnectionResetError:
-                pass
-            except BrokenPipeError:
-                pass
-            finally:
-                conn.close()
-                print(f"Camera client disconnected (I-{self._instance})")
+            # delay at sample rate
+            while self.robot.getTime() - start_time < self.color_camera.getSamplingPeriod() / 1000:
+                time.sleep(0.001)
 
     def get_camera_frame(self) -> np.ndarray:
         """Get the RGB image from the camera as a numpy array of bytes"""
-        img = self.camera.getImage()
+        img = self.color_camera.getImage()
 
         if self._camera_data_type == "uint8":
             data_type = np.uint8
@@ -444,25 +377,25 @@ class WebotsArduVehicle:
         else:
             raise ValueError("Unsupported data type specified")
 
-        img = np.frombuffer(img, data_type).reshape((self.camera.getHeight(), self.camera.getWidth(), 4))
+        img = np.frombuffer(img, data_type).reshape((self.color_camera.getHeight(), self.color_camera.getWidth(), 4))
         return img[:, :, :3] # RGB only, no Alpha
 
-    def get_rangefinder_frame(self) -> np.ndarray:
+    def get_rangefinder_frame(self, rangefinder) -> np.ndarray:
         """Get the rangefinder depth image as a numpy array of int8 or int16"""\
 
         # get range image size
-        height = self.rangefinder.getHeight()
-        width = self.rangefinder.getWidth()
+        height = rangefinder.getHeight()
+        width = rangefinder.getWidth()
 
         # get image, and convert raw ctypes array to numpy array
         # https://cyberbotics.com/doc/reference/rangefinder
-        image_c_ptr = self.rangefinder.getRangeImage(data_type="buffer")
+        image_c_ptr = rangefinder.getRangeImage(data_type="buffer")
         img_arr = np.ctypeslib.as_array(image_c_ptr, (width*height,))
         img_floats = img_arr.reshape((height, width))
 
         # normalize and set unknown values to max range
-        range_range = self.rangefinder.getMaxRange() - self.rangefinder.getMinRange()
-        img_normalized = (img_floats - self.rangefinder.getMinRange()) / range_range
+        range_range = rangefinder.getMaxRange() - rangefinder.getMinRange()
+        img_normalized = (img_floats - rangefinder.getMinRange()) / range_range
         img_normalized[img_normalized == float('inf')] = 1
 
         # convert to int8 or int16, allowing for the option of higher precision if desired
